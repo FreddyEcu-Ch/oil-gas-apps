7f59c2725d405495d52f5be567e52165775a1663
fredd <freddypcarrion@gmail.com>
2022-06-07 10:29:16 -0500

    Poes function

---
+++ poes/model/poes.py
@@ -0,0 +1,22 @@
+def poes(area, h, poro, swi, boi):
+    """
+
+    Parameters
+    ----------
+    area
+        area, acres
+    h
+        Thickness, ft
+    poro
+        Porosity, fraction
+    swi
+        Water saturation, fraction
+    boi
+        Bubble point factor, rb/stb
+
+    Returns
+        float number -> poes
+    -------
+
+    """
+    return 7758 * area * poro * h (1 - swi) / boi
